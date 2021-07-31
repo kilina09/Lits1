6199fb4677660f072792a3260900338b24ae27ec
Your Name <verritka@ukr.net>
2021-07-31 13:15:48 +0300

    initial commit

---
+++ src/com/lits/Main.java
@@ -0,0 +1,27 @@
+package com.lits;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // System.out.println("Hello world!");
+        /*
+        /*
+        double b = 125;
+        double b1 = 2;
+        System.out.println("b + b1 = " + (b / b1));
+        */
+        int b = 124;
+        int b1 = 2;
+        System.out.println("b + b1 = " + (b % b1));
+
+        //System.out.println("1");
+
+
+        char c = 't';
+        System.out.println((char) 101);
+
+        */
+        //int i = 10;
+        var i = 10f;
+    }
+}
